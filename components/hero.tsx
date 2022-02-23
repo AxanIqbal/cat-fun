a0d8cadd66c8b5f8122987013eaa127bb1219287
Ahsan Iqbal <axan.iqbal1234@gmail.com>
2022-02-23 23:44:03 +0500

    update hero image

--- components/hero.tsx
+++ components/hero.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Image from 'next/image'
 import HeroImage from '../assets/hero.jpeg'
 import styled from "@emotion/styled";
+import Button from "./button";
 
 const HeroImageStyled = styled(Image)`
   background-size: cover;
@@ -10,13 +11,30 @@ const HeroImageStyled = styled(Image)`
 
 const Section = styled.section`
     //margin-top: 10px;
-  background-color: #0070f3;
+  background-color: #5363FF;
+  position: relative;
+  overflow: clip;
+`
+
+const MyButton = styled(Button)`
+  position: absolute;
+  bottom: 1%;
+  right: -2%;
+  z-index: 10;
+  color: whitesmoke;
+  font-size: 2rem;
+  width: 50%;
+  
+  &:hover {
+    background-color: #5363FF;
+  }
 `
 
 function Hero() {
     return (
         <Section>
             <HeroImageStyled alt={"Hero Image"} src={HeroImage} />
+            <MyButton backColor={"#5363FF"}>Join Now</MyButton>
         </Section>
     );
 }
